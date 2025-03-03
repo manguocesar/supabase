cd4fb12647f134e2726fc9268127b90cba84fe23
Cesar <cesar.hertz@icloud.com>
2025-03-03 11:29:32 +0100

    feat: add navigation links and improve instruments display

--- app/instruments/page.tsx
+++ app/instruments/page.tsx
@@ -1,8 +1,29 @@
 import { createClient } from '@/utils/supabase/server';
+import Link from 'next/link';
 
 export default async function Instruments() {
     const supabase = await createClient();
     const { data: instruments } = await supabase.from("instruments").select();
 
-    return <pre>{JSON.stringify(instruments, null, 2)}</pre>
+    return (
+        <div className='flex flex-col space-between'>
+            <Link href='/' className='bg-gray-400 rounded-md p-2'>
+                <p>Home</p>
+            </Link>
+            <div className='m-4 flex flex-col space-y-4'>
+                {instruments ? (
+                    instruments.map(instrument => (
+                        <p key={instrument.id}>o {instrument.name}</p>
+                    ))
+                ) : (
+                    <p>No instruments available</p>
+
+                )}
+                <pre className='text-sm'>
+                    JSON Data structure:
+                    {JSON.stringify(instruments, null, 2)}
+                </pre>
+            </div>
+        </div>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import Link from "next/link";
 
 export default async function Home() {
   return (
@@ -9,6 +10,9 @@ export default async function Home() {
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        <Link href='/instruments' className='bg-gray-400 w-4/12 rounded-md p-2'>
+          <p>o View Instruments</p>
+        </Link>
         {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
       </main>
     </>
